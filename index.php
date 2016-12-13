5dee414970ddc881864111169ddabed59781e718
liteng <zyhdbteng@icoud.com>
2016-12-13 10:44:52 +0800

    add elasticsearch

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo phpinfo();die;
 define('APPLICATION_PATH', dirname(__FILE__));
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
---
+++ test_search.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liteng
+ * Date: 2016/12/9
+ * Time: 下午5:02
+ */
